5bdadeaea0971cdc8ebf71892926a99e43e32438
piyush-n <nandhapk@rknec.edu>
2023-12-12 18:23:24 +0530

    Final Commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT =2424;
+export const  mongDbURL=
+"mongodb+srv://Piyush-system:Piyush_Password@cluster0.bkk5wlp.mongodb.net/?retryWrites=true&w=majority"
---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import  express  from "express";
+import {PORT,mongDbURL} from './config.js';
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/bookRoute.js";
+import cors from 'cors';
+
+const app=express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    return res.status(244).send("hello");
+})
+
+//option 1 allow all origin with default of cors
+app.use(cors());
+
+//option 2 allow custom origin
+// app.use(
+//     cors({
+//         origin:'http://localhost:3000',
+//         methods:['GET','POST','PUT','DELETE'],
+//         allowedHeaders:['Content-Type'],
+//     })
+// );
+
+app.use('/books',booksRoute);
+mongoose.connect(mongDbURL)
+.then(()=>{
+    console.log('app connected to db');
+    app.listen(PORT,()=>{
+        console.log(`working at port no :${PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(err);
+});
